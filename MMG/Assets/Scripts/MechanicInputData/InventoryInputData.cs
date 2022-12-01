a0078c8e85b08ce74ef998062fdb16e590792411
kalebtaylor3 <taylorkaleb16@gmail.com>
2022-12-01 18:44:44 -0500

    Revert "Forgot to delete a few things in Inventory class"
    This reverts commit 1fe2bacc0b30dd6c9049298f8c8f4dcf341113f6.

--- MMG/Assets/Scripts/MechanicInputData/InventoryInputData.cs
+++ MMG/Assets/Scripts/MechanicInputData/InventoryInputData.cs
@@ -4,6 +4,11 @@ using MMG;
 [CreateAssetMenu(fileName = "InventoryInputData", menuName = "MMG_Player/Data/InventoryInputData")]
 public class InventoryInputData : ScriptableObject
 {
+    private bool item1ChangeClick;
+    private bool item2ChangeClick;
+    private bool item3ChangeClick;
+    private bool item4ChangeClick;
+    private bool putItemAway;
 
     private bool openSpellBook;
 
@@ -11,5 +16,35 @@ public class InventoryInputData : ScriptableObject
     {
         get => openSpellBook;
         set => openSpellBook = value;
+    }
+
+    public bool Change1Clicked
+    {
+        get => item1ChangeClick;
+        set => item1ChangeClick = value;
+    }
+
+    public bool Change2Clicked
+    {
+        get => item2ChangeClick;
+        set => item2ChangeClick = value;
+    }
+
+    public bool Change3Clicked
+    {
+        get => item3ChangeClick;
+        set => item3ChangeClick = value;
+    }
+
+    public bool Change4Clicked
+    {
+        get => item4ChangeClick;
+        set => item4ChangeClick = value;
+    }
+
+    public bool PutItemAway
+    {
+        get => putItemAway;
+        set => putItemAway = value;
     }
 }
