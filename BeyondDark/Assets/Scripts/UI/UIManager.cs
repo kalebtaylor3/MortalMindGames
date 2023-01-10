994d5e2396492b7471e07ee5a17744e05a362c6e
Kaleb Taylor <taylorkaleb16@gmail.com>
2023-01-10 16:09:57 -0500

    cuz niam said so

--- BeyondDark/Assets/Scripts/UI/UIManager.cs
+++ BeyondDark/Assets/Scripts/UI/UIManager.cs
@@ -10,7 +10,7 @@ public class UIManager : MonoBehaviour
 
     private void OnEnable()
     {
-        staminaSlider = GetComponent<Slider>();
+        //staminaSlider = GetComponent<Slider>();
     }
 
     //STAMINA
