24ed271fb419955ce92e9826dbeef911d92a5536
NaimHeredia <heredia_naim@hotmail.com>
2023-02-02 16:50:19 -0500

    Conceal Death, pls send help

--- BeyondDark/Assets/Scripts/Global/WorldData.cs
+++ BeyondDark/Assets/Scripts/Global/WorldData.cs
@@ -45,6 +45,7 @@ public class WorldData : MonoBehaviour
     [SerializeField] InputController input;
     [SerializeField] GameObject playerDeathMR;
     [SerializeField] GameObject vorgonModel;
+    [SerializeField] public GameObject fadeOut;
 
     // FOR AI
     [SerializeField] List<Section> sections = null;
@@ -131,28 +132,41 @@ public class WorldData : MonoBehaviour
             //dont call exit. first check if door is open. if door is open use the animator to play the open animation. wait for lenght of animation. then set jumpscare active then wait for lenght wait for for how ever many seconds are before fade to black. Before fade to black starts switch the cameras
             //lastConceal.enteranceAnimator.SetTrigger("Enter");
             //yield return new WaitForSeconds(lastConceal.enteranceAnimator.GetCurrentAnimatorClipInfo(0).Length);
-            playerDeathMR.SetActive(true);
+            //playerDeathMR.SetActive(true);
+            lastConceal.ToggleConcealDeath();
             vorgonModel.SetActive(false);
-            yield return new WaitForSeconds(1);
-            lastConceal.ToggleCameraExit();
+
+            yield return new WaitForSeconds(1.0f);
+            fadeOut.SetActive(true);
+            lastConceal.ToggleCamChange();
+            TpTest.Instance.MortalRealmDeath(pickUpCP);            
+            yield return new WaitForSeconds(4);
+            fadeOut.SetActive(false);
+
             //lastConceal.ExitArea();
             //yield return new WaitForSeconds(2.5f);
-            
+
         }
         else
         {
             playerDeathMR.SetActive(true);
-            vorgonModel.SetActive(false);
+            vorgonModel.SetActive(false);            
+            yield return new WaitForSeconds(1);
+            TpTest.Instance.MortalRealmDeath(pickUpCP);
+            fadeOut.SetActive(true);
+            playerDeathMR.SetActive(false);
+            yield return new WaitForSeconds(1);
+            fadeOut.SetActive(false);
         }
 
 
-        yield return new WaitForSeconds(1);
-        TpTest.Instance.MortalRealmDeath(pickUpCP);
-        //yield return new WaitForSeconds(1);
-        playerDeathMR.SetActive(false);
-        vorgonModel.SetActive(true);
-
         
+        //yield return new WaitForSeconds(1);
+        
+        //playerDeathMR.SetActive(false);
+        vorgonModel.SetActive(true);
+        //yield return new WaitForSeconds(1);
+        //fadeOut.SetActive(false);
 
         lastConceal = null;
         input.canMove = true;
--- BeyondDark/Assets/Scripts/InteractionSystem/Interactables/ConcelableAreaInteractable.cs
+++ BeyondDark/Assets/Scripts/InteractionSystem/Interactables/ConcelableAreaInteractable.cs
@@ -22,6 +22,8 @@ namespace MMG
         Vector3 lookAtStartPosition;
         Quaternion startRotation;
 
+        public GameObject deathCAA;
+
         public enum clamp { X, Y, Z };
         public clamp cameraClamp;
         [SerializeField] public float maxLocalRotationValue;
@@ -303,22 +305,43 @@ namespace MMG
             canCreak = false;
         }
 
-        public void ToggleCameraExit()
+        public void ToggleConcealDeath()
+        {
+            StartCoroutine(ToggleDeath());
+        }
+
+        public void ToggleCamChange()
         {
             playerCamera.gameObject.SetActive(true);
             concelableAreaCam.gameObject.SetActive(false);
+        }
+
+        IEnumerator ToggleDeath()
+        {
+            deathCAA.SetActive(true);
+            yield return new WaitForSeconds(1.5f);
+
+
+            //playerCamera.gameObject.SetActive(true);
+            //concelableAreaCam.gameObject.SetActive(false);
+
+            //WorldData.Instance.fadeOut.SetActive(true);
             canCreak = false;
             isHidding = false;
             happenOnce = false;
             input.canMove = true;
             OnLeaveSpot?.Invoke();
+            deathCAA.SetActive(false);
+            enteranceAnimator.SetTrigger("Inside");
+            //yield return new WaitForSeconds(enteranceAnimator.GetCurrentAnimatorClipInfo(0).Length + 1);
+            
         }
 
         IEnumerator WaitForExit()
         {
             yield return new WaitForSeconds(0.9f);
             //Move Player
-            player.transform.position = searchPos.position;
+            //player.transform.position = searchPos.position;
             //playerCamera.LookAt = lookAtTransform;
 
             playerCamera.gameObject.SetActive(true);
