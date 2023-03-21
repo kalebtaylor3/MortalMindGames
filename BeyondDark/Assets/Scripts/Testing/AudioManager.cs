a160d10f99b9eda8b47cff5695a259e94502187e
NaimHeredia <heredia_naim@hotmail.com>
2023-03-20 23:29:58 -0400

    Chase music

---
+++ BeyondDark/Assets/Scripts/Testing/AudioManager.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource mainSource;
+    //public AudioSource introSource;
+    public AudioSource chaseSource;
+
+    public VorgonController vorgon;
+
+    public float fadeTimer = 3f;
+
+    //public List<AudioClip> clips;
+
+    private void OnEnable()
+    {
+        ChaseState.onStartChase += StartChaseMusic;
+        ChaseState.onEndChase += EndChaseMusic;
+        WorldData.OnDeath += EndChaseMusic;
+    }
+
+    private void OnDisable()
+    {
+        ChaseState.onStartChase -= StartChaseMusic;
+        ChaseState.onEndChase -= EndChaseMusic;
+        WorldData.OnDeath -= EndChaseMusic;
+    }
+
+    private void Update()
+    {
+        if(chaseSource.isPlaying && !vorgon.inChase)
+        {
+            EndChaseMusic();
+        }
+    }
+
+    public void StartChaseMusic()
+    {
+        if(WorldData.Instance.activeRealm == WorldData.REALMS.MORTAL)
+        {
+            if (vorgon.inChase && !chaseSource.isPlaying)
+            {
+                StopAllCoroutines();
+                StartCoroutine(FadeSource(mainSource, chaseSource));
+            }
+        }       
+    }
+
+    public void EndChaseMusic()
+    {
+        if (WorldData.Instance.activeRealm == WorldData.REALMS.MORTAL)
+        {
+            if (!vorgon.inChase)
+            {
+                StartCoroutine(endChaseMusic());
+            }
+        }             
+    }
+
+    private IEnumerator endChaseMusic()
+    {
+        yield return new WaitForSeconds(3.0f);
+
+        if(!vorgon.inChase && chaseSource.isPlaying)
+        {
+            StopAllCoroutines();
+            StartCoroutine(FadeSource(chaseSource, mainSource));
+        }
+        
+    }
+
+    private IEnumerator FadeSource(AudioSource playing, AudioSource notPlaying)
+    {
+
+        float clockT = 0f;
+
+        notPlaying.Play();
+
+        while (clockT < fadeTimer)
+        {
+            notPlaying.volume = Mathf.Lerp(0, 0.5f, clockT / fadeTimer);
+            playing.volume = Mathf.Lerp(0.5f, 0, clockT / fadeTimer);
+
+            clockT += Time.deltaTime;
+            clockT = Mathf.Clamp(clockT, 0, fadeTimer);
+            yield return null;
+        }
+        playing.Stop();
+    }
+}
