567b4f0d5ad82fffde9c95542299fea0c2babbba
NaimHeredia <heredia_naim@hotmail.com>
2023-04-04 01:08:18 -0400

    New Stamina Slider (fade)

--- BeyondDark/Assets/Scripts/UI/UIManager.cs
+++ BeyondDark/Assets/Scripts/UI/UIManager.cs
@@ -8,9 +8,86 @@ public class UIManager : MonoBehaviour
     //Stamina
     [SerializeField] private Slider staminaSlider;
 
+    [SerializeField] private Image fillImage;    
+
+    public float fadeTimer = 0.5f;
+
+    public bool fading = false;
+    public bool fadedOut = true;
+
     private void OnEnable()
     {
         //staminaSlider = GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        if (staminaSlider.value == 1 && !fading && !fadedOut) 
+        {
+            fading = true;
+            StopAllCoroutines();
+            StartCoroutine(FadeSlider(false, fillImage.color.a));
+        }
+        else if (staminaSlider.value < 1 && !fading && fadedOut) 
+        {
+            if (WorldData.Instance.player.movementInputData.IsRunning)
+            {
+                fading = true;
+                StopAllCoroutines();
+                StartCoroutine(FadeSlider(true, fillImage.color.a));
+            }          
+        }
+
+        if (staminaSlider.value < 1 && fading && !fadedOut && !WorldData.Instance.player.movementInputData.IsRunning)
+        {
+            fading = true;
+            StopAllCoroutines();
+            StartCoroutine(FadeSlider(false, fillImage.color.a));
+        }
+    }
+
+    public IEnumerator FadeSlider(bool flag, float f = 1)
+    {
+        float clockT = 0f;
+
+        Color newC = fillImage.color;
+
+        if (flag)
+        {
+            fadedOut = false;
+            while (clockT < fadeTimer)
+            {
+                newC.a = Mathf.Lerp(0f, 1f, clockT / fadeTimer);
+
+                clockT += Time.deltaTime;
+                clockT = Mathf.Clamp(clockT, 0, fadeTimer);
+                yield return null;
+                fillImage.color = newC;
+            }
+
+            newC.a = 1f;
+
+            fillImage.color = newC;
+        }
+        else
+        {
+            fadedOut = true;
+            while (clockT < fadeTimer)
+            {
+                newC.a = Mathf.Lerp(f, 0, clockT / fadeTimer);
+
+                clockT += Time.deltaTime;
+                clockT = Mathf.Clamp(clockT, 0, fadeTimer);
+                yield return null;
+                fillImage.color = newC;
+            }
+
+            newC.a = 0f;
+
+            fillImage.color = newC;
+        }
+
+        fading = false;
     }
 
     //STAMINA
