1af2ee5c98bcbcd531e4f83ae6b921ab3ecf92fe
kalebtaylor3 <taylorkaleb16@gmail.com>
2023-04-03 20:58:08 -0400

    acid damages you

--- BeyondDark/Assets/Scripts/Player/MassDamage.cs
+++ BeyondDark/Assets/Scripts/Player/MassDamage.cs
@@ -9,10 +9,27 @@ public class MassDamage : MonoBehaviour
 
     public bool canDamagePlayer = true;
 
-    private void OnTriggerStay(Collider other)
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if (other.gameObject.tag == "VorgonRealmPlayer" && canDamagePlayer)
+    //    {
+    //        //Debug.Log("MeleeAttacked");
+    //        PlayerHealthSystem.Instance.currentPlayerHealth -= damage;
+    //        PlayerHealthSystem.Instance.TakeDamage();
+    //        Rumbler.Instance.RumbleConstant(0, 0.2f, 0.4f);
+    //    }
+    //}
+
+    private void OnParticleCollision(GameObject other)
     {
+
+        //int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
+
+        Debug.Log(other.name);
+
         if (other.gameObject.tag == "VorgonRealmPlayer" && canDamagePlayer)
         {
+            Debug.Log("Spit hit");
             //Debug.Log("MeleeAttacked");
             PlayerHealthSystem.Instance.currentPlayerHealth -= damage;
             PlayerHealthSystem.Instance.TakeDamage();
