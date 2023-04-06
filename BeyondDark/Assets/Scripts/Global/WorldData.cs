2e3688a9b7b3a41db46568666341010feca91c6a
NaimHeredia <heredia_naim@hotmail.com>
2023-04-06 01:04:22 -0400

    for build

--- BeyondDark/Assets/Scripts/Global/WorldData.cs
+++ BeyondDark/Assets/Scripts/Global/WorldData.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting.Antlr3.Runtime.Misc;
-using UnityEditor.ShortcutManagement;
+//using UnityEditor.ShortcutManagement;
 using UnityEngine;
 using UnityEngine.Video;
 using static RelicSpawnManager;
