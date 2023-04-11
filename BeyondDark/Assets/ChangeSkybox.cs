3acc42c03c82f351e4c8dcf93cda9c6a213ab696
kalebtaylor3 <taylorkaleb16@gmail.com>
2023-04-11 06:30:12 -0400

    Update ChangeSkybox.cs

--- BeyondDark/Assets/ChangeSkybox.cs
+++ BeyondDark/Assets/ChangeSkybox.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class ChangeSkybox : MonoBehaviour
 {
     public Material mortalRealmSkybox;
+    public GameObject mortalRealmPPE;
+    public GameObject vorgonRealmPPE;
 
     private void OnEnable()
     {
         RenderSettings.skybox = mortalRealmSkybox;
+        mortalRealmPPE.SetActive(true);
     }
 
     private void Update()
     {
         RenderSettings.skybox = mortalRealmSkybox;
+        mortalRealmPPE.SetActive(true);
+        vorgonRealmPPE.SetActive(false);
     }
 }
